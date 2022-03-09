2c689108276ea54d8b55bf9e88b8684810ec949e
Charles Peace <charlespeace@hotmail.com>
2022-03-09 14:19:38 -0800

    connected javascript and cleaned up code for each employee type. wrote prompt functions in index.js

--- index.js
+++ index.js
@@ -1,48 +1,123 @@
 
-const Employee = require('./lib/Employee.js');
+const Employee = require('./lib/Employee.js')
+const Manager = require('./lib/Manager.js')
+const Engineer = require('./lib/Engineer.js')
+const Intern = require('./lib/Intern.js')
 
-new Employee().getName();
+const inquirer = require('inquirer')
+
+teamMembers = []
+
+function newTeam() {
+    function newEmployee() {
+        inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What is the role of this team member?',
+                choices: ['Manager', 'Engineer', 'Intern']
+            }
+        ])
+        .then(selection => {
+            if (selection.role === 'Manager') {
+                addManager()
+            } else if (selection.role === 'Engineer') {
+                addEngineer()
+            } else if (selection.role === 'Intern') {
+                addIntern()
+            }
+        })
+    }
+    function addManager() {
+        inquirer
+        .prompt([    
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the manager you would like to add?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is their employee ID?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is their email address?'
+            },
+            { 
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is the office number of this manager?'
+            }
+        ])
+        .then(answers => {
+            const employee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+            teamMembers.push(employee)
+        })
+    }
+    function addEngineer() {
+        inquirer
+        .prompt([    
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the engineer you would like to add?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is their employee ID?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is their email address?'
+            },
+            { 
+                type: 'input',
+                name: 'github',
+                message: 'What is their github username?'
+            }
+        ])
+        .then(answers => {
+            const employee = new Engineer(answers.name, answers.id, answers.email, answers.github)
+            teamMembers.push(employee)
+        })
+    }
+    function addIntern() {
+        inquirer
+        .prompt([    
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the intern you would like to add?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is their employee ID?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is their email address?'
+            },
+            { 
+                type: 'input',
+                name: 'school',
+                message: 'What is school does the intern attend?'
+            }
+        ])
+        .then(answers => {
+            const employee = new Intern(answers.name, answers.id, answers.email, answers.school)
+            teamMembers.push(employee)
+        })
+    }
+    newEmployee()
+}
+newTeam()
 
 
-// const inquirer = require('inquirer');
-// const fs = require('fs');
-
-
-// const generatePage = () => { 
-//     return `
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Team Roster</title>
-// </head>
-// <body>
-//     <header>
-//         <h1>My team</h1>
-//     </header>
-//     <main>
-//         // ${Employee.name}(js)
-//         // ${Employee.email}(js)
-//     </main>
-// </body>
-// </html>
-//     `;
-// };
-
-// const writeToFile = (generatePage) => {
-//     fs.writeFile('./dist/index.html', generatePage, err => {
-//         if (err) throw new Error(err);
-//         console.log('File created!');
-//     });
-// };
-
-
-// questions()
-//     .then(data => {
-//         return generatePage(data);
-//     })
-//     .then(generatePage => {
-//         return writeToFile(generatePage);
-//     })
