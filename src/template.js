f5c2c6c72759edec15b5f96a8c586d9e8d0d9351
Charles Peace <charlespeace@hotmail.com>
2022-03-28 16:29:05 -0700

    added simple styling and checked to see that prompt/page generation works

--- src/template.js
+++ src/template.js
@@ -2,9 +2,9 @@ const generatePage = members => {
     const team = []
     const createManager = manager => {
         return `
-        <article>
-            <div>${manager.getName()}</div>
-            <div>(${manager.getRole()})</div>
+        <article class="card">
+            <h2 class="header">${manager.getName()}</h2>
+            <div class="header">(${manager.getRole()})</div>
             <div>ID: ${manager.getId()}</div>
             <div>Email: ${manager.getEmail()}</div>
             <div>Office Number: ${manager.getOfficeNumber()}</div>
@@ -13,20 +13,20 @@ const generatePage = members => {
     }
     const createEngineer = engineer => {
         return `
-        <article>
-            <div>${engineer.getName()}</div>
-            <div>(${engineer.getRole()})</div>
+        <article class="card">
+            <h2 class="header">${engineer.getName()}</h2>
+            <div class="header">(${engineer.getRole()})</div>
             <div>ID: ${engineer.getId()}</div>
-            <div>$Email: {engineer.getEmail()}</div>
+            <div>$Email: ${engineer.getEmail()}</div>
             <div>Github: ${engineer.getGithub()}</div>
         </article>
         `
     }
     const createIntern = intern => {
         return `
-        <article>
-            <div>${intern.getName()}</div>
-            <div>(${intern.getRole()})</div>
+        <article class="card">
+            <h2 class="header">${intern.getName()}</h2>
+            <div class="header">(${intern.getRole()})</div>
             <div>${intern.getId()}</div>
             <div>${intern.getEmail()}</div>
             <div>${intern.getSchool()}</div>
@@ -48,13 +48,13 @@ module.exports = members => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team Roster</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3">
+    <link rel="stylesheet" href="../src/style.css">
 </head>
 <body>
-    <header>
+    <header class="header">
         <h1>My team</h1>
     </header>
-    <main>
+    <main class="contentpanel">
         <section>
             <div>
                 <div>
